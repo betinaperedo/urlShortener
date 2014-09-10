6c9841799da221189a9fb8955609a0b614602eef
betina.peredo <betina.peredo@tecso.coop>
2014-09-09 21:39:16 -0300

    Singleton class added

--- src/main/java/coop/tecso/main/servlet/UrlShortenerServlet.java
+++ src/main/java/coop/tecso/main/servlet/UrlShortenerServlet.java
@@ -1,4 +1,4 @@
-package coop.tecso.main;
+package coop.tecso.main.servlet;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
@@ -16,29 +16,35 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.common.io.ByteStreams;
 
+import coop.tecso.main.db.PersistentMap;
+
 @WebServlet(urlPatterns = { "/*" })
 public class UrlShortenerServlet extends HttpServlet {
 
 	public static final String CREATE_URL = "/create";
-	public static final String INDEX = "/";
+	public static final String INDEX_PATH = "/";
+	public static final String INDEX_PAGE = "index.jsp";
+	public static final String INTERNAL_ERROR = "Missing urlinput parameter";
+	public static final String URL_PARAMETER = "urlinput";
 
 	public final Map<String, String> urlMap = Maps.newHashMap();
+	public PersistentMap dataBase = PersistentMap.getInstance();
 	public int count;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String urlinput = req.getParameter("urlinput");
+		String urlinput = req.getParameter(URL_PARAMETER);
 
-		checkArgument(!Strings.isNullOrEmpty(urlinput), "Missing urlinput parameter");
+		checkArgument(!Strings.isNullOrEmpty(urlinput), INTERNAL_ERROR);
 
 		String urlOuput = getShortUrl(urlinput);
 		persistsUrlMapping(urlinput, urlOuput);
 		if (req.getPathInfo().equals(CREATE_URL)) {
-			String htmlRespone = "<html>";
-			urlOuput = "http://localhost:8080/urlshortener/" + urlOuput;
-			htmlRespone += "<h2>Your short url is : " + urlOuput + "</h2>";
-			htmlRespone += "</html>";
+			
+			
+			String htmlRespone = buildAnswer(urlOuput);
+			
 			resp.getWriter().append(htmlRespone);
 		}
 	}
@@ -46,13 +52,13 @@ public class UrlShortenerServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		if (req.getPathInfo().equals(INDEX)) {
+		if (req.getPathInfo().equals(INDEX_PATH)) {
 			ByteStreams.copy(
-					req.getServletContext().getResourceAsStream("index.jsp"),
+					req.getServletContext().getResourceAsStream(INDEX_PAGE),
 					resp.getOutputStream());
 		} else {
-			checkState(req.getPathInfo().startsWith("/"));
-			String redirectUrl = urlMap.get(req.getPathInfo().substring(1));
+			checkState(req.getPathInfo().startsWith(INDEX_PATH));
+			String redirectUrl = dataBase.select(req.getPathInfo().substring(1));
 			resp.sendRedirect("http://" + redirectUrl);
 		}
 	}
@@ -60,9 +66,18 @@ public class UrlShortenerServlet extends HttpServlet {
 	private String getShortUrl(String url) {
 		return Integer.toString(count++);
 	}
+	private String buildAnswer(String urlOuput) {
+		StringBuilder sb = new StringBuilder("<html>");
+		sb.append("<h2>Your short url is : ");
+		sb.append("http://localhost:8080/urlshortener/");
+		sb.append(urlOuput);
+		sb.append("</html>");
+		return sb.toString();
+		
+	}
 
 	private void persistsUrlMapping(String longUrl, String shortUrl) {
-		urlMap.put(shortUrl, longUrl);
+		dataBase.insert(shortUrl, longUrl);
 	}
 
 }
